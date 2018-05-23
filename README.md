1a2a72ad2e28acae9b2625535f10c1443c901b30
Samuel Marks <samuelmarks@gmail.com>
2018-05-23 15:38:16 +1000

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+offregister_service
+===============
+This package follows the offregister specification to handle services.
+
+## Install dependencies
+
+    pip install -r requirements.txt
+
+## Install package
+
+    pip install .
---
+++ offregister_service/ubuntu/__init__.py
@@ -0,0 +1,16 @@
+from offregister_fab_utils.ubuntu.systemd import install_upgrade_service, restart_systemd
+
+
+def install_service0(conf_name, *args, **kwargs):
+    install_upgrade_service(
+        conf_name,
+        conf_local_filepath=kwargs.get('systemd-conf-file'),
+        context={
+            'ExecStart': kwargs['ExecStart'],
+            'Environments': kwargs['Environments'],
+            'WorkingDirectory': kwargs['WorkingDirectory'],
+            'User': kwargs['User'],
+            'Group': kwargs['Group']
+        }
+    )
+    return restart_systemd(conf_name)
