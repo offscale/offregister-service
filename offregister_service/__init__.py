29d54ad38cba63366ff076d7d87dacffbf5f1bb0
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_service/__init__.py
+++ offregister_service/__init__.py
@@ -6,12 +6,12 @@ import logging
 from os import path
 from logging.config import dictConfig as _dictConfig
 
-__author__ = 'Samuel Marks'
-__version__ = '0.0.1'
+__author__ = "Samuel Marks"
+__version__ = "0.0.1"
 
 
 def get_logger(name=None):
-    with open(path.join(path.dirname(__file__), '_data', 'logging.yml'), 'rt') as f:
+    with open(path.join(path.dirname(__file__), "_data", "logging.yml"), "rt") as f:
         data = yaml.load(f)
     _dictConfig(data)
     return logging.getLogger(name=name)
